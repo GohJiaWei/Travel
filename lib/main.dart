298e0d9e749dbb6230feada1e4b3728ac4ecce83
Goh Jia Wei <gjw2920@gmail.com>
2024-08-03 13:40:54 +0800

    nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:travel/pages/getstarted.dart';
+import 'package:travel/pages/home.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: GetStarted(),
+      home: HomeScreen(),
     );
   }
 }
