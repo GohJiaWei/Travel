56b3c8fed08850af2a036d5f003d92830667cb51
LooHuaiYuan <loohuaiyuan@gmail.com>
2024-08-03 18:09:48 +0800

    5

--- lib/schedule/schedule.dart
+++ lib/schedule/schedule.dart
@@ -1,14 +1,162 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
-class SchedulePage extends StatelessWidget {
+
+class SchedulePage extends StatefulWidget {
   const SchedulePage({super.key});
 
   @override
+  State<SchedulePage> createState() => _SchedulePageState();
+}
+
+class _SchedulePageState extends State<SchedulePage> {
+  final locationController = Location();
+
+  static const googlePlex = LatLng(37.4223, -122.0848);
+  static const mountainView = LatLng(37.3861, -122.0839);
+
+  LatLng? currentPosition;
+  Map<PolylineId, Polyline> polylines = {};
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance
+        .addPostFrameCallback((_) async => await initializeMap());
+  }
+
+  Future<void> initializeMap() async {
+    await fetchLocationUpdates();
+  }
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('SchedulePage'),
+      body: Column(
+        children: [
+          SizedBox(height: 60,),
+          Center(
+
+            child: Container(
+              width: 400,
+              height: 200,
+              child: GoogleMap(
+                initialCameraPosition:
+                CameraPosition(
+                target: googlePlex,
+                zoom: 10,
+              ),
+                markers: {
+                  const Marker(
+                    markerId: MarkerId('sourceLocation'),
+                    icon: BitmapDescriptor.defaultMarker,
+                    position: googlePlex,
+                  ),
+                  const Marker(
+                    markerId: MarkerId('destinationLocation'),
+                    icon: BitmapDescriptor.defaultMarker,
+                    position: mountainView,
+                  )
+                },
+              ),
+            ),
+          ),
+          SizedBox(height: 10,),
+          Expanded(
+            child: ListView.builder(
+                itemBuilder: (BuildContext context, int index){
+                  return GestureDetector(
+                    onTap: (){
+
+                    },
+                    child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(left: 8.0),
+                              child: Text('9.00 am to 5.00 pm'),
+                            ),
+                            SizedBox(height: 3,),
+                            Container(
+                                decoration: BoxDecoration(
+                                  color: Color(0xFFBFB4AD),
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 8),
+                                  child: ListTile(
+                                    leading: Image.network(
+                                      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYArSfFlMh4VhBz3tYLefzqGLnDtzeSulxxQ&s',
+                                      width: 175.0, // Width of the image
+                                      height: 250.0, // Height of the image
+                                      fit: BoxFit.cover, // How the image should be inscribed into the box
+                                    ),
+                                    title: Text('The Enchanted Nightingale'),
+                                  ),
+                                )
+                            ),
+                            SizedBox(height: 8,)
+                          ],
+                        )
+                    ),
+                  );
+                },
+
+                itemCount: 5
+            ),
+          ),
+        ],
       ),
     );
   }
+
+  Future<void> fetchLocationUpdates() async {
+    bool serviceEnabled;
+    PermissionStatus permissionGranted;
+
+    serviceEnabled = await locationController.serviceEnabled();
+    if (serviceEnabled) {
+      serviceEnabled = await locationController.requestService();
+    } else {
+      return;
+    }
+
+    permissionGranted = await locationController.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await locationController.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    locationController.onLocationChanged.listen((currentLocation) {
+      if (currentLocation.latitude != null &&
+          currentLocation.longitude != null) {
+        setState(() {
+          currentPosition = LatLng(
+            currentLocation.latitude!,
+            currentLocation.longitude!,
+          );
+        });
+      }
+    });
+  }
+
+
+
+  Future<void> generatePolyLineFromPoints(
+      List<LatLng> polylineCoordinates) async {
+    const id = PolylineId('polyline');
+
+    final polyline = Polyline(
+      polylineId: id,
+      color: Colors.blueAccent,
+      points: polylineCoordinates,
+      width: 5,
+    );
+
+    setState(() => polylines[id] = polyline);
+  }
+
 }
